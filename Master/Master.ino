118fe73099fa61d500d31bc57f48c8d2064cd41a
trantieubinh <rms.titanic536@gmail.com>
2020-12-30 13:10:27 +0700

    Update Master.ino

--- Master/Master.ino
+++ Master/Master.ino
@@ -8,50 +8,57 @@
 byte spi_receiver_1;
 byte spi_receiver_2;
 byte spi_receiver_3;
-
+long speedcar;
+byte byteSend = 120;
+char command;
 void setup() {
   Serial.begin(9600);
-  pinMode(SS,OUTPUT);
+  pinMode(SS, OUTPUT);
   pinMode(SS1, OUTPUT);
   pinMode(SS2, OUTPUT);
   pinMode(SS3, OUTPUT);
   digitalWrite(SS1, HIGH);
   digitalWrite(SS2, HIGH);
   digitalWrite(SS3, HIGH);
- // SPCR |= _BV(MSTR);
+//  SPCR |= _BV(MSTR);
   SPI.begin ();
 }
+int mapping(long speedcar)
+{
+  return map(speedcar, 0, 4000, 0, 255);
+}
+void sendData(int sending, int slave)
+{
+  switch (slave)
+  {
+    case 1:
+      { digitalWrite(SS1, LOW);
+        spi_receiver_1 = SPI.transfer(sending);
+        digitalWrite(SS1, HIGH);
+        break;
+      }
+    case 2:
+      { digitalWrite(SS2, LOW);
+        spi_receiver_2 = SPI.transfer(sending);
+        digitalWrite(SS2, HIGH);
+        break;
+      }
+    case 3:
+      { digitalWrite(SS3, LOW);
+        spi_receiver_2 = SPI.transfer(sending);
+        digitalWrite(SS3, HIGH);
+        break;
+      }
+  }
+}
+
 
 void loop() {
-  long value=9000;
-  int sending=map(value,0,9000,0,255);
-digitalWrite(SS2, LOW);
-spi_receiver_2 = SPI.transfer (sending); 
-digitalWrite(SS2, HIGH);
+  sendData(70,2);
+  delay(1000);
+  sendData(40,3);
+  delay(1000);
+  sendData(140,3);
+  delay(1000);
 
-digitalWrite(SS3, LOW);
-   spi_receiver_3 = SPI.transfer (110); 
-digitalWrite(SS3, HIGH);
-delay(50);
-digitalWrite(SS3, LOW);
-   spi_receiver_3 = SPI.transfer (40); 
-digitalWrite(SS3, HIGH);
- delay(1000);    
-  
-  /*digitalWrite(ss1, LOW);
-    digitalWrite(ss2, LOW);
-    spi_receiver_1 = SPI.transfer ('A');
-    spi_receiver_2 = SPI.transfer ('A');
-    delay(1500);
-    spi_receiver_1 = SPI.transfer ('C');
-    spi_receiver_2 = SPI.transfer ('C');
-    delay(1000);
-    spi_receiver_1 = SPI.transfer ('A');
-    spi_receiver_2 = SPI.transfer ('A');
-    delay(1500);
-    spi_receiver_1 = SPI.transfer ('C');
-    spi_receiver_2 = SPI.transfer ('C');
-    delay(1000);
-    digitalWrite(ss1, HIGH);
-    digitalWrite(ss2, HIGH);*/
 }
